4dbb37ea6cbf0b13700236d38e176a4a971f199c
Guidolin Marco <m.guidolin@egeatech.com>
2021-05-18 15:16:29 +0200

    ADD custom interface to let the repository know which methods to use

---
+++ src/Interfaces/Models/SupportsQueryBuilderInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Interfaces\Models;
+
+use Spatie\QueryBuilder\AllowedSort;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\AllowedInclude;
+
+interface SupportsQueryBuilderInterface
+{
+    public static function getDefaultSortingField(): string;
+
+    /**
+     * @return AllowedFilter[]
+     */
+    public static function getAllowedFilters(): array;
+
+    /**
+     * @return AllowedInclude[]
+     */
+    public static function getAllowedIncludes(): array;
+
+    /**
+     * @return AllowedSort[]
+     */
+    public static function getAllowedSorting(): array;
+
+    /**
+     * @return string[]
+     */
+    public static function getAllowedAppendAttributes(): array;
+}
